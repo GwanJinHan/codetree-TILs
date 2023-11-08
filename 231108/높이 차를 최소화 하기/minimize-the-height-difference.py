630744d0ca1ac8cd195b6f5037c02fec30af817b
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-11-08 18:03:49 +0900

    Title: 높이 차를 최소화 하기, Time: 395ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 231108/높이 차를 최소화 하기/minimize-the-height-difference.py
@@ -0,0 +1,48 @@
+# BFS + 이진탐색
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = [
+    list(map(int, input().split())) for _ in range(n)
+]
+
+dy, dx = [0, 1, 0, -1], [1, 0, -1, 0]
+
+
+left, right = 0, 500
+
+answer = sys.maxsize
+
+while left <= right:
+    mid = (left + right) // 2
+
+    queue = deque([(0, 0, arr[0][0], arr[0][0])])
+    visit = [[sys.maxsize] * m for _ in range(n)]
+    ans = 0
+    while queue:
+        r, c, M, N = queue.popleft()
+
+        ans = max(ans, M - N)
+
+        if visit[r][c] <= M - N:
+            continue
+        
+        visit[r][c] = M - N
+
+        for i in range(4):
+            nr, nc = r + dy[i], c + dx[i]
+
+            if 0 <= nr < n and 0 <= nc < m and M - N < visit[nr][nc]:
+                if max(M, arr[nr][nc]) - min(N, arr[nr][nc]) <= mid:
+                    queue.append((nr, nc, max(M, arr[nr][nc]), min(N, arr[nr][nc])))
+
+    if visit[n - 1][m - 1] != sys.maxsize:
+        right = mid - 1
+        answer = min(answer, ans)
+    else:
+        left = mid + 1
+
+print(answer)
