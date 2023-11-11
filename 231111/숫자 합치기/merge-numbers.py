9ac58e24b97df89e0dfe96285c4b74df07a549e9
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-11-11 17:48:24 +0900

    Title: 숫자 합치기, Time: 318ms, Memory: 31MB, Status: Passed - CodeTree

--- 231111/숫자 합치기/merge-numbers.py
+++ 231111/숫자 합치기/merge-numbers.py
@@ -1,14 +1,14 @@
-from heapq import heappush, heappop
+from heapq import heappush, heappop, heapify
 
 n = int(input())
 arr = list(map(int, input().split()))
-
+heapify(arr)
 ans = 0
 while len(arr) != 1:
     a = heappop(arr)
     b = heappop(arr)
 
     heappush(arr, a + b)
-    ans += a + b
+    ans += (a + b)
 
 print(ans)
