f4fb1b246327352649f049ebad6a190115df1f38
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-11-14 02:33:28 +0900

    Title: 각 정점까지의 최단 경로, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231114/각 정점까지의 최단 경로/shortest-path-to-each-vertex.py
@@ -0,0 +1,33 @@
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+k = int(input())
+INF = sys.maxsize
+
+arr = [[] for _ in range(n)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    arr[a - 1].append((b - 1, c))
+    arr[b - 1].append((a - 1, c))
+
+queue = [(0, k - 1)]
+d = [INF] * n
+d[k - 1] = 0
+
+while queue:
+    w, i = heappop(queue)
+
+    if d[i] != w:
+        continue
+    
+    for node, weight in arr[i]:
+        if d[i] + weight < d[node]:
+            d[node] = d[i] + weight
+            heappush(queue, (d[node], node))
+
+
+for val in d:
+    print(val)
