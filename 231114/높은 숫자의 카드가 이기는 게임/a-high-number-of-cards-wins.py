29774baef7f2b1abbb3d9ac39f393db795a02ff1
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-11-14 13:32:34 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 134ms, Memory: 26MB, Status: Passed - CodeTree

--- 231114/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 231114/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -1,21 +1,22 @@
-# 최대한 큰 점수를 내는 법 : 크게 지고 작게 이긴다
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 
-arr = [int(input()) for _ in range(n)]
+arr = [int(input()) for _ in range(n)] + [2 * n + 1]
 arr.sort()
 
 ans = 0
 prev = 1
-for i in range(1, n):
+
+for i in range(1, n + 1):
     if arr[i - 1] + 1 != arr[i] :
         if arr[i] - arr[i - 1] - 1 > prev:
             ans += prev
+            prev = 1
         else:
             ans += arr[i] - arr[i - 1] - 1 
-        prev = 1
+            prev -= arr[i] - arr[i - 1] - 2
     else:
         prev += 1
 
