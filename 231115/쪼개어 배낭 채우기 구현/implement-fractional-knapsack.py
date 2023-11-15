93a0e821c7cdbbe8572a8ba2989c0470f98c0420
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-11-15 17:53:49 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 511ms, Memory: 30MB, Status: Passed - CodeTree

---
+++ 231115/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,27 @@
+from heapq import heappop, heappush
+import sys
+input = sys.stdin.readline
+n, m = tuple(map(int, input().split()))
+
+arr = [ ] 
+for _ in range(n):
+    w, v = tuple(map(int, input().split()))
+    heappush(arr, (-v/w, w, v)) #내림차순으로
+
+result = 0
+while m > 0:
+    try: 
+        v_per_w, w, v = heappop(arr)
+        v_per_w = - v_per_w
+    except:
+        break
+
+    if m >= w:
+        m -= w
+        result += v_per_w * w
+    else:
+        result += (v_per_w * m)
+        m = 0
+
+    
+print(f'{result:.3f}')
