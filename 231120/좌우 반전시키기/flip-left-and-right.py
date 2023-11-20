fa92f5b3e1b7437ebd155ca42460ab28631aa54c
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-11-20 13:39:17 +0900

    Title: 좌우 반전시키기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231120/좌우 반전시키기/flip-left-and-right.py
@@ -0,0 +1,30 @@
+# 첫 번째 위치 누르기 불가
+# 왼, 누, 오 반전
+# 모두 1
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+# 00 -> 오른쪽 0 누르기
+# 01 -> 오른쪽 1 누르기
+def switch(num):
+    return 0 if num == 1 else 1
+
+ans = 0
+flag = False
+for i in range(n):
+    if arr[i] == 0:
+        if flag: # 00
+            arr[i] = switch(arr[i])
+            arr[i - 1] = switch(arr[i - 1])
+            ans += 1
+            flag = False
+        else:
+            flag = True
+    else:
+        if flag: # 01
+            arr[i] = switch(arr[i])
+            arr[i - 1] = switch(arr[i - 1])
+            ans += 1
+    
+print(ans if all(arr) else -1)
