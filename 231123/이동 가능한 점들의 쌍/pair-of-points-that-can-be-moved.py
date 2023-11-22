d54bd9b9db8714fbd745d01b172059ca9e67e7f2
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-11-23 01:11:49 +0900

    Title: 이동 가능한 점들의 쌍, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231123/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+INF = sys.maxsize
+n, m, p, q = map(int, input().split())
+matrix = [[INF] * n for _ in range(n)]
+
+for i in range(n):
+    matrix[i][i] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    matrix[a - 1][b - 1] = c
+
+for inter in range(n):
+    for r in range(n):
+        for c in range(n):
+            if matrix[r][c] > matrix[r][inter] + matrix[inter][c]:
+                matrix[r][c] = matrix[r][inter] + matrix[inter][c]
+
+cnt = 0
+acc = 0
+for _ in range(q):
+    a, b = map(int, input().split())
+
+    tmp = INF
+    for i in range(q):
+        tmp = min(tmp, matrix[a - 1][i] + matrix[i][b - 1])
+    
+    if tmp < INF:
+        cnt += 1
+        acc += tmp
+
+print(cnt)
+print(acc)
