91af504c8605047c8b6e9e6219047a45f85f2f58
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-11-23 18:33:47 +0900

    Title: 이동 가능한 점들의 쌍, Time: 244ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231123/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
+++ 231123/이동 가능한 점들의 쌍/pair-of-points-that-can-be-moved.py
@@ -4,6 +4,7 @@ input = sys.stdin.readline
 INF = sys.maxsize
 n, m, p, q = map(int, input().split())
 matrix = [[INF] * n for _ in range(n)]
+matrix_red = [[False] * n for _ in range(n)]
 
 for i in range(n):
     matrix[i][i] = 0
@@ -12,24 +13,30 @@ for _ in range(m):
     a, b, c = map(int, input().split())
     matrix[a - 1][b - 1] = c
 
+for i in range(p):
+    for j in range(n):
+        matrix_red[j][i] = True
+        matrix_red[i][j] = True
+
 for inter in range(n):
     for r in range(n):
         for c in range(n):
             if matrix[r][c] > matrix[r][inter] + matrix[inter][c]:
                 matrix[r][c] = matrix[r][inter] + matrix[inter][c]
+                if matrix_red[r][inter] or matrix_red[inter][c]:
+                    matrix_red[r][c] = True
+                elif not matrix_red[r][inter] and not matrix_red[inter][c]:
+                    matrix_red[r][c] = False
+            elif matrix[r][c] == matrix[r][inter] + matrix[inter][c] and (matrix_red[r][inter] or matrix_red[inter][c]):
+                matrix_red[r][c] = True
 
 cnt = 0
 acc = 0
 for _ in range(q):
     a, b = map(int, input().split())
-
-    tmp = INF
-    for i in range(p):
-        tmp = min(tmp, matrix[a - 1][i] + matrix[i][b - 1])
-    
-    if tmp < INF:
+    if matrix_red[a - 1][b - 1] and matrix[a - 1][b - 1] != INF:
         cnt += 1
-        acc += tmp
+        acc += matrix[a - 1][b - 1]
 
 print(cnt)
 print(acc)
