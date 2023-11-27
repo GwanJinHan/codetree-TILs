118c2f64ed3439873b14aa33539cdd697ff3f895
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2023-11-27 20:41:24 +0900

    Title: 최단 거리 3, Time: 306ms, Memory: 30MB, Status: Passed - CodeTree

---
+++ 231127/최단 거리 3/shortest-distance-3.py
@@ -0,0 +1,33 @@
+import sys
+from heapq import heappush, heappop
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n)]
+INF = sys.maxsize
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a - 1].append((b - 1, c))
+    graph[b - 1].append((a - 1, c))
+
+s, e = map(int, input().split())
+d = [INF] * n
+d[s - 1] = 0
+path = [0] * n
+
+hq = [(0, s - 1)]
+
+while hq:
+    w, i = heappop(hq)
+
+    if d[i] < w:
+        continue
+    
+    for node, weight in graph[i]:
+        if d[node] >= weight + d[i]:
+            d[node] = weight + d[i]
+            heappush(hq, (d[node], node))
+            path[node] = i
+
+print(d[e - 1])
