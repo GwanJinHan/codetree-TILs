9f8866e529986f80507e09704ea01b3caf16399b
GwanJinHan <42578300+GwanJinHan@users.noreply.github.com>
2024-02-06 09:39:54 +0900

    Title: 사각형 채우기 2, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 240206/사각형 채우기 2/rectangle-fill-2.py
+++ 240206/사각형 채우기 2/rectangle-fill-2.py
@@ -9,8 +9,8 @@ def square(num):
         return 0
 
     t1 = memo[num + 1] if num + 1 < n and memo[num + 1] != -1 else square(num + 1)
-    t2 = memo[num + 2] if num + 2 < n and memo[num + 2] != -1 else square(num + 2) * 2
-    t = t1 + t2
+    t2 = memo[num + 2] if num + 2 < n and memo[num + 2] != -1 else square(num + 2)
+    t = t1 + t2 * 2
     memo[num] = t
     return t
 
